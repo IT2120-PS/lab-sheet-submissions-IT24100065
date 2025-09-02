d2c057a09bd786a15057c56f325f476d20c52236
IT24100065 <IT24100065@my.sliit.lk>
2025-09-02 14:46:25 +0530

    Lab 06

---
+++ Lab Sheet 06/Lab 06.R
@@ -0,0 +1,15 @@
+getwd()
+setwd("C:\\Users\\IT24100065\\Desktop\\IT24100065 Lab 06")
+getwd()
+
+#Exercise
+#part1
+# i. Biominal distribution
+# ii.
+1-pbinom(46,50,0.85,lower.tail = TRUE)
+
+#part2
+# i.The number of calls received in an hour 
+# ii.Poisson Distribution
+# iii.
+dpois(15,12)
